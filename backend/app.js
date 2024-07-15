baf899481b6a1667a66f56d866779dcf3377102a
Maharoof Rashi <rashimon083@gmail.com>
2024-07-15 13:35:23 +0400

    MongoDB connection is working and Server is running 😊

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const connectDB = require('./config/db')
 
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("Hello rashi!")
